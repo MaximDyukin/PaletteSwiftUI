358787517a3be9c35f000a1af9001de7fab398a8
Max <maximdyukin@gmail.com>
2021-10-30 15:26:24 +0500

    Initial Commit

---
+++ PaletteSwiftUI/PaletteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletteSwiftUIApp.swift
+//  PaletteSwiftUI
+//
+//  Created by Maxim Dukin on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct PaletteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
